2a560d75205b5d82096d278c8bb3ba1aa8ec8f08
Ivan Golikov <igolikov@octoberry.ru>
2019-02-09 13:16:37 +0300

    Initial commit

---
+++ src/batch_shortlinker/core.clj
@@ -0,0 +1,7 @@
+(ns batch-shortlinker.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/batch_shortlinker/core_test.clj
@@ -0,0 +1,7 @@
+(ns batch-shortlinker.core-test
+  (:require [clojure.test :refer :all]
+            [batch-shortlinker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
